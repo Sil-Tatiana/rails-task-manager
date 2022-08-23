a9ab987c567f002e7dfb72aa8c8ca338b99412c7
Silvia Neves <siltatiana.sn@gmail.com>
2022-08-23 16:01:06 +0100

    Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index', as: :index
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  patch 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_103357) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
-    t.boolean "false"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
